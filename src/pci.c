82d2831ea5a196acd917d8533c751d55db84ca75
Oliver O'Halloran <oliver.ohalloran@oracle.com>
2021-07-15 23:56:51 +1000

    pci: Use C code to trawl sysfs rather than shelling out
    Busybox's 'dirname' errors out when given multiple command line
    arguments while the coreutils version is more forgiving. Replace
    the shelling out with readdir() and friends.
    
    Signed-off-by: Oliver O'Halloran <oliver.ohalloran@oracle.com>

--- src/pci.c
+++ src/pci.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2018,2019 IBM Corp.
+// Copyright (C) 2021, Oracle and/or its affiliates.
 
 #define _GNU_SOURCE
 #include <assert.h>
@@ -11,55 +12,87 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #include "pci.h"
 #include "shell.h"
 
+int read_sysfs_id(int dirfd, const char *file)
+{
+	char id_string[7];
+	int rc, fd;
+	long id;
+
+	fd = openat(dirfd, file, 0);
+	if (fd < 0)
+		return -1;
+
+	memset(id_string, 0, sizeof(id_string));
+
+	/* vdid format is: "0xABCD" */
+	rc = read(fd, id_string, 6);
+	close(fd);
+
+	if (rc < 0)
+		return -1;
+
+	id = strtol(id_string, NULL, 16);
+	if (id > 0xffff || id < 0)
+		return -1;
+	return id;
+}
+
 int pci_open(uint16_t vid, uint16_t did, int bar)
 {
-        char cmd_stdout[1024];
-        struct stat statbuf;
-        char *cmd, *end;
-        char *cmd_fmt;
         char *res;
         int rc;
         int fd;
+	int found = 0;
 
-        cmd_fmt = "/bin/bash -c \"comm -12 "
-                  "<(grep -l 0x%x /sys/bus/pci/devices/*/vendor|xargs dirname)"
-                  " "
-                  "<(grep -l 0x%x /sys/bus/pci/devices/*/device|xargs dirname)"
-                  "\"";
+	struct dirent *de;
+	int dfd;
+	DIR *d;
+	char path[300]; /* de->d_name has a max of 255, and add some change */
 
-        rc = asprintf(&cmd, cmd_fmt, vid, did);
-        if (rc == -1)
-                return -errno;
+	d = opendir("/sys/bus/pci/devices/");
+	if (!d)
+		exit(1);
 
-        rc = shell_get_output(cmd, &cmd_stdout[0], sizeof(cmd_stdout));
-        free(cmd);
-        if (rc < 0)
-                return rc;
+	dfd = dirfd(d); /* we need an FD for openat() */
 
-        assert(rc < sizeof(cmd_stdout));
+	while ((de = readdir(d))) {
+		int this_vid, this_did;
 
-        /* Open the first match, because ¯\_(ツ)_/¯ - assume we only have one */
-        end = strchr(cmd_stdout, '\n');
-        if (!end)
-                return -EIO;
+		if (de->d_type != DT_LNK)
+			continue;
 
-        *end = '\0';
+		snprintf(path, sizeof(path), "%s/vendor", de->d_name);
+		this_vid = read_sysfs_id(dfd, path);
 
-        /* Just check if we found anything */
-        rc = stat(cmd_stdout, &statbuf);
-        if (rc < 0)
-                return -errno;
+		snprintf(path, sizeof(path), "%s/device", de->d_name);
+		this_did = read_sysfs_id(dfd, path);
 
-        rc = asprintf(&res, "%s/resource%d", cmd_stdout, bar);
-        if (rc == -1)
-                return -errno;
+		if (this_vid == vid && this_did == did) {
+			found = 1;
+			break;
+		}
+	}
 
-        fd = open(res, O_RDWR | O_SYNC);
-        free(res);
+	/* NB: dfd and de->d_name are both invalidated by closedir() */
+	if (!found) {
+		closedir(d);
+		return -ENOENT;
+	}
+
+	rc = asprintf(&res, "%s/resource%d", de->d_name, bar);
+	if (rc == -1) {
+		closedir(d);
+		return -errno;
+	}
+
+	fd = openat(dfd, res, O_RDWR | O_SYNC);
+	free(res);
+	closedir(d);
 
         return fd;
 }
