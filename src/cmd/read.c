9f68e622c3624d055374e3e70c74b5aa75328e63
Tan Siewert <tan@siewert.io>
2025-07-29 22:07:34 +0200

    cmd: read: rework to use argp and common connection struct
    cmdline has been slightly changed, and the the internals were reworked
    to use argp and the common connection struct.
    
    The region to be read from is now provided via the `--type` option.
    
    Before: culvert read ram 0x0 32
    After: culvert read -t ram 0x0 32
    
    Signed-off-by: Tan Siewert <tan@siewert.io>

--- src/cmd/read.c
+++ src/cmd/read.c
@@ -5,6 +5,7 @@
 #include "ast.h"
 #include "cmd.h"
 #include "compiler.h"
+#include "connection.h"
 #include "flash.h"
 #include "host.h"
 #include "log.h"
@@ -13,6 +14,7 @@
 #include "soc/sdmc.h"
 #include "soc/sfc.h"
 
+#include <argp.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -20,7 +22,93 @@
 #include <string.h>
 #include <unistd.h>
 
-static int cmd_read_firmware(int argc, char *argv[])
+static char cmd_read_args_doc[] =
+    "--type=<firmware|ram> [<ADDRESS> <LENGTH>] "
+    "[via DRIVER [INTERFACE [IP PORT USERNAME PASSWORD]]]";
+
+static char cmd_read_doc[] =
+    "\n"
+    "Read command"
+    "\v"
+    "NOTE: Only the 'ram' type can parse address and length!\n\n"
+    "All data will be written to stdout.\n\n"
+    "Supported types:\n"
+    "  firmware    Read the content from the FMC\n"
+    "  ram         Read the content from the memory\n";
+
+enum cmd_read_mode {
+    none,
+    firmware,
+    ram,
+};
+
+static struct argp_option cmd_read_options[] = {
+    { "type", 't', "TYPE", 0, "Type to be read from", 0 },
+    {0},
+};
+
+struct cmd_read_args {
+    unsigned long mem_base;
+    unsigned long mem_size;
+    struct connection_args connection;
+    enum cmd_read_mode mode;
+};
+
+static error_t cmd_read_parse_opt(int key, char *arg, struct argp_state *state)
+{
+    struct cmd_read_args *arguments = state->input;
+    int rc;
+
+    switch (key) {
+    case 't':
+        if (strcmp(arg, "firmware") && strcmp(arg, "ram"))
+            argp_error(state, "Invalid type '%s'", arg);
+
+        if (!strcmp(arg, "firmware"))
+            arguments->mode = firmware;
+        else
+            arguments->mode = ram;
+        break;
+    case ARGP_KEY_ARG:
+        if (!strcmp(arg, "via")) {
+            rc = cmd_parse_via(state->next - 1, state, &arguments->connection);
+            if (rc != 0)
+                argp_error(state, "Failed to parse connection arguments. Returned code %d", rc);
+            break;
+        }
+
+        /* If mode is not ram, skip any further args */
+        if (arguments->mode != ram)
+            break;
+
+        if (state->arg_num == 0)
+            parse_mem_arg(state, "read RAM base", &arguments->mem_base, arg);
+        else if (state->arg_num == 1)
+            parse_mem_arg(state, "read RAM size", &arguments->mem_size, arg);
+
+        break;
+    case ARGP_KEY_END:
+        if (arguments->mode == none)
+            argp_error(state, "No type to be read from defined...");
+        /* It is possible to not pass the ram base and size */
+        if (arguments->mode == ram && (state->arg_num > 0 && state->arg_num < 2))
+            argp_error(state, "Not enough arguments for ram mode...");
+        break;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
+
+static struct argp cmd_read_argp = {
+    .options = cmd_read_options,
+    .parser = cmd_read_parse_opt,
+    .args_doc = cmd_read_args_doc,
+    .doc = cmd_read_doc,
+};
+
+static int cmd_read_firmware(struct cmd_read_args *arguments)
 {
     struct host _host, *host = &_host;
     struct soc _soc, *soc = &_soc;
@@ -32,7 +120,7 @@ static int cmd_read_firmware(int argc, char *argv[])
     int cleanup;
     int rc;
 
-    if ((rc = host_init(host, argc, argv)) < 0) {
+    if ((rc = host_init(host, &arguments->connection)) < 0) {
         loge("Failed to initialise host interfaces: %d\n", rc);
         return rc;
     }
@@ -86,56 +174,19 @@ cleanup_host:
     return rc;
 }
 
-static int cmd_read_ram(int argc, char *argv[])
+static int cmd_read_ram(struct cmd_read_args *arguments)
 {
     struct host _host, *host = &_host;
     struct soc _soc, *soc = &_soc;
     struct soc_region dram, vram;
-    unsigned long start, length;
     struct sdmc *sdmc;
     struct ahb *ahb;
-    char *endp;
     int rc;
 
-    /* FIXME: doesn't handle bridge argument parsing */
-    if (argc >= 2) {
-        errno = 0;
-        start = strtoul(argv[0], &endp, 0);
-        if (start == ULONG_MAX && errno) {
-            loge("Failed to parse RAM region start address '%s': %s\n", argv[0], strerror(errno));
-            return -errno;
-#if ULONG_MAX > UINT32_MAX
-        } else if (start > UINT32_MAX) {
-            loge("RAM region start address '%s' exceeds address space\n", argv[0]);
-            return -EINVAL;
-#endif
-        }
-        argc--;
-        argv++;
-
-        errno = 0;
-        length = strtoul(argv[0], &endp, 0);
-        if (length == ULONG_MAX && errno) {
-            loge("Failed to parse RAM region length '%s': %s\n", argv[0], strerror(errno));
-            return -errno;
-#if ULONG_MAX > UINT32_MAX
-        } else if (length > UINT32_MAX) {
-            loge("RAM region length '%s' exceeds address space\n", argv[0]);
-            return -EINVAL;
-#endif
-        }
-        argc--;
-        argv++;
-    } else {
-        start = 0;
-        length = 0;
-    }
-
-    if (UINT32_MAX - length < start) {
+    if (UINT32_MAX - arguments->mem_size < arguments->mem_base)
         return -EINVAL;
-    }
 
-    if ((rc = host_init(host, argc, argv)) < 0) {
+    if ((rc = host_init(host, &arguments->connection)) < 0) {
         loge("Failed to initialise host interfaces: %d\n", rc);
         return rc;
     }
@@ -159,28 +210,31 @@ static int cmd_read_ram(int argc, char *argv[])
         goto cleanup_soc;
     }
 
-    if (start && length) {
-        if (start < dram.start || (start + length) > (dram.start + dram.length)) {
+    if (arguments->mem_base && arguments->mem_size) {
+        if (arguments->mem_base < dram.start ||
+            (arguments->mem_base + arguments->mem_size) > (dram.start + dram.length)) {
+            loge("Ill-formed RAM region provided for write\n");
             rc = -EINVAL;
             goto cleanup_soc;
         }
 
         logi("Dumping %" PRId32 "MiB (%#.8" PRIx32 "-%#.8" PRIx32 ")",
-             length >> 20, start, start + length - 1);
+             arguments->mem_size >> 20, arguments->mem_base,
+             arguments->mem_base + arguments->mem_size - 1);
     } else {
         if ((rc = sdmc_get_vram(sdmc, &vram))) {
             goto cleanup_soc;
         }
 
-        start = dram.start;
-        length = dram.length - vram.length;
+        arguments->mem_base = dram.start;
+        arguments->mem_size = dram.length - vram.length;
 
         logi("%dMiB DRAM with %dMiB VRAM; dumping %dMiB (0x%x-0x%08x)\n",
              dram.length >> 20, vram.length >> 20,
              (dram.length - vram.length) >> 20, dram.start, vram.start - 1);
     }
 
-    rc = soc_siphon_out(soc, start, length, STDOUT_FILENO);
+    rc = soc_siphon_out(soc, arguments->mem_base, arguments->mem_size, STDOUT_FILENO);
     if (rc) {
         errno = -rc;
         perror("soc_siphon_in");
@@ -195,30 +249,36 @@ cleanup_host:
     return rc;
 }
 
-static int do_read(const char *name __unused, int argc, char *argv[])
+static int do_read(int argc, char **argv)
 {
     int rc;
 
-    if (argc < 1) {
-        loge("Not enough arguments for read command\n");
-        return EXIT_FAILURE;
-    }
+    struct cmd_read_args arguments = {0};
+    arguments.mode = none;
+    rc = argp_parse(&cmd_read_argp, argc, argv, ARGP_IN_ORDER, 0, &arguments);
+    if (rc != 0)
+        return rc;
 
-    if (!strcmp("firmware", argv[0])) {
-        rc = cmd_read_firmware(argc - 1, argv + 1);
-    } else if (!strcmp("ram", argv[0])) {
-        rc = cmd_read_ram(argc - 1, argv + 1);
-    } else {
-        loge("Unsupported read type '%s'", argv[0]);
+    switch (arguments.mode) {
+    case firmware:
+        rc = cmd_read_firmware(&arguments);
+        break;
+    case ram:
+        rc = cmd_read_ram(&arguments);
+        break;
+    /* If it reaches none here, then the argument parse logic is broken. */
+    case none:
+        loge("read: Reached 'none' mode after argument parsing. This is a bug!\n");
         rc = -EINVAL;
+        break;
     }
 
-    return rc < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+    return rc;
 }
 
 static const struct cmd read_cmd = {
-    "read",
-    "<firmware|ram ADDRESS LENGTH> [INTERFACE [IP PORT USERNAME PASSWORD]]",
-    do_read,
+    .name = "read",
+    .description = "Read data from the FMC or RAM",
+    .fn = do_read,
 };
 REGISTER_CMD(read_cmd);
