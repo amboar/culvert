6ca07d50e0406c916d05ef63c8a2cd3e77aea901
Tan Siewert <tan@siewert.io>
2026-03-02 17:19:50 +0100

    clk: abstract clock ops per compatible
    In order to support multiple platforms using different code paths,
    the functions must be moved into SoC-specific ones, and the generic
    functions will call the defined functions in `clk_ops`.
    
    Signed-off-by: Tan Siewert <tan@siewert.io>

--- src/soc/clk.c
+++ src/soc/clk.c
@@ -12,111 +12,138 @@
 #include <errno.h>
 #include <stdint.h>
 
+#define	SCU_CLK_STOP			0x0c
+#define		SCU_CLK_STOP_UART3		BIT(25)
 
-#define SCU_CLK_STOP                    0x0c
-#define   SCU_CLK_STOP_UART3            BIT(25)
-#define SCU_HW_STRAP			0x70
-#define   SCU_HW_STRAP_UART_DBG_SEL	(1 << 29)
-#define   SCU_HW_STRAP_SIO_DEC          (1 << 20)
-#define   SCU_HW_STRAP_ARM_CLK          (1 <<  0)
-#define SCU_SILICON_REVISION		0x7c
+#define	SCU_HW_STRAP				0x70
+#define		SCU_HW_STRAP_UART_DBG_SEL	BIT(29)
+#define		SCU_HW_STRAP_CLKIN_IN_MOD	BIT(23)
+#define		SCU_HW_STRAP_SIO_DEC		BIT(20)
+#define		SCU_HW_STRAP_ARM_CLK		BIT(0)
+
+#define	SCU_SILICON_REVISION		0x7c
+
+struct clk_ops {
+	int64_t (*rate_ahb)(struct clk *ctx);
+	int (*disable)(struct clk *ctx, enum clksrc src);
+	int (*enable)(struct clk *ctx, enum clksrc src);
+};
 
 struct clk {
 	struct scu *scu;
+	const struct clk_ops *ops;
 };
-
-static int64_t clk_rate_ahb(struct clk *ctx)
-{
-    static const uint32_t cpu_freqs_24_48[] = {
-	384000000,
-	360000000,
-	336000000,
-	408000000
-    };
-    static const uint32_t cpu_freqs_25[] = {
-	400000000,
-	375000000,
-	350000000,
-	425000000
-    };
-    static const uint32_t ahb_div[] = { 1, 2, 4, 3 };
-    uint32_t strap, cpu_clk, div;
-    int rc;
-
-    /* HW strapping gives us the CPU freq and AHB divisor */
-    if ((rc = scu_readl(ctx->scu, SCU_HW_STRAP, &strap)) < 0)
-        return rc;
-
-    if (strap & 0x00800000)
-	cpu_clk = cpu_freqs_25[(strap >> 8) & 3];
-    else
-	cpu_clk = cpu_freqs_24_48[(strap >> 8) & 3];
-
-    div = ahb_div[(strap >> 10) & 3];
-
-    return cpu_clk / div;
-}
 
 int64_t clk_get_rate(struct clk *ctx, enum clksrc src)
 {
-    if (src != clk_ahb)
-        return -ENOTSUP;
+	if (src != clk_ahb)
+		return -ENOTSUP;
 
-    return clk_rate_ahb(ctx);
+	return ctx->ops->rate_ahb(ctx);
 }
 
 int clk_disable(struct clk *ctx, enum clksrc src)
 {
-    uint32_t reg;
-    int rc;
-
-    switch (src) {
-    case clk_arm:
-        return scu_writel(ctx->scu, SCU_HW_STRAP, SCU_HW_STRAP_ARM_CLK);
-    case clk_uart3:
-        if ((rc = scu_readl(ctx->scu, SCU_CLK_STOP, &reg)) < 0)
-            return rc;
-
-        reg |= SCU_CLK_STOP_UART3;
-
-        if ((rc = scu_writel(ctx->scu, SCU_CLK_STOP, reg)) < 0)
-            return rc;
-
-        return 0;
-    default:
-        break;
-    }
-
-    return -ENOTSUP;
+	return ctx->ops->disable(ctx, src);
 }
 
 int clk_enable(struct clk *ctx, enum clksrc src)
 {
-    uint32_t reg;
-    int rc;
-
-    switch (src) {
-    case clk_arm:
-        return scu_writel(ctx->scu, SCU_SILICON_REVISION, SCU_HW_STRAP_ARM_CLK);
-    case clk_uart3:
-        if ((rc = scu_readl(ctx->scu, SCU_CLK_STOP, &reg)) < 0)
-            return rc;
-
-        reg &= ~SCU_CLK_STOP_UART3;
-
-        if ((rc = scu_writel(ctx->scu, SCU_CLK_STOP, reg)) < 0)
-            return rc;
-
-        return 0;
-    default:
-        break;
-    }
-
-    return -ENOTSUP;
+	return ctx->ops->enable(ctx, src);
 }
 
+static int64_t ast2500_clk_rate_ahb(struct clk *ctx)
+{
+	static const uint32_t cpu_freqs_24_48[] = {
+		384000000,
+		360000000,
+		336000000,
+		408000000
+	};
+
+	static const uint32_t cpu_freqs_25[] = {
+		400000000,
+		375000000,
+		350000000,
+		425000000
+	};
+
+	static const uint32_t ahb_div[] = { 1, 2, 4, 3 };
+	uint32_t strap, cpu_clk, div;
+	int rc;
+
+	/* HW strapping gives us the CPU freq and AHB divisor */
+	if ((rc = scu_readl(ctx->scu, SCU_HW_STRAP, &strap)) < 0)
+		return rc;
+
+	if (strap & SCU_HW_STRAP_CLKIN_IN_MOD)
+		cpu_clk = cpu_freqs_25[(strap >> 8) & 3];
+	else
+		cpu_clk = cpu_freqs_24_48[(strap >> 8) & 3];
+
+	div = ahb_div[(strap >> 10) & 3];
+
+	return cpu_clk / div;
+}
+
+int ast2500_clk_disable(struct clk *ctx, enum clksrc src)
+{
+	uint32_t reg;
+	int rc;
+
+	switch (src) {
+	case clk_arm:
+		return scu_writel(ctx->scu, SCU_HW_STRAP, SCU_HW_STRAP_ARM_CLK);
+	case clk_uart3:
+		if ((rc = scu_readl(ctx->scu, SCU_CLK_STOP, &reg)) < 0)
+			return rc;
+
+		reg |= SCU_CLK_STOP_UART3;
+
+		if ((rc = scu_writel(ctx->scu, SCU_CLK_STOP, reg)) < 0)
+			return rc;
+
+		return 0;
+	default:
+		break;
+	}
+
+	return -ENOTSUP;
+}
+
+int ast2500_clk_enable(struct clk *ctx, enum clksrc src)
+{
+	uint32_t reg;
+	int rc;
+
+	switch (src) {
+	case clk_arm:
+		return scu_writel(ctx->scu, SCU_SILICON_REVISION, SCU_HW_STRAP_ARM_CLK);
+	case clk_uart3:
+		if ((rc = scu_readl(ctx->scu, SCU_CLK_STOP, &reg)) < 0)
+			return rc;
+
+		reg &= ~SCU_CLK_STOP_UART3;
+
+		if ((rc = scu_writel(ctx->scu, SCU_CLK_STOP, reg)) < 0)
+			return rc;
+
+		return 0;
+	default:
+		break;
+	}
+
+	return -ENOTSUP;
+}
+
+static const struct clk_ops ast2500_clk_ops = {
+	.rate_ahb = ast2500_clk_rate_ahb,
+	.disable = ast2500_clk_disable,
+	.enable = ast2500_clk_enable,
+};
+
 static const struct soc_device_id clk_match[] = {
-    { .compatible = "aspeed,ast2500-clock" },
+    { .compatible = "aspeed,ast2500-clock", .data = &ast2500_clk_ops },
     { },
 };
 
@@ -133,6 +160,13 @@ static int clk_driver_init(struct soc *soc, struct soc_device *dev)
     ctx->scu = scu_get(soc);
     if (!ctx->scu) {
         rc = -ENODEV;
+        goto cleanup_ctx;
+    }
+
+    ctx->ops = soc_device_get_match_data(soc, clk_match, &dev->node);
+    if (!ctx->ops) {
+        loge("Failed to find clk ops\n");
+        rc = -EINVAL;
         goto cleanup_ctx;
     }
 
