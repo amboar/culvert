7c44761e7d4a2a63dafe3082fe8414bffa480545
Tan Siewert <tan@siewert.io>
2026-03-21 22:41:34 +0100

    bits: add field macros
    Signed-off-by: Tan Siewert <tan@siewert.io>

--- src/bits.h
+++ src/bits.h
@@ -4,7 +4,9 @@
 #ifndef _BITS_H
 #define _BITS_H
 
-#define BIT(x)		(1UL << (x))
-#define GENMASK(h, l)	((BIT(h) | (BIT(h) - 1)) & ~(BIT(l) - 1))
+#define BIT(x)			(1UL << (x))
+#define GENMASK(h, l)		((BIT(h) | (BIT(h) - 1)) & ~(BIT(l) - 1))
+#define FIELD_PREP(mask, val)	(((val) << __builtin_ctz(mask)) & (mask))
+#define FIELD_GET(mask, val)	(((val) & (mask)) >> __builtin_ctz(mask))
 
 #endif
