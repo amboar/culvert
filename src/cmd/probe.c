amboar/culvert

commit a120c4fa9a8cdaf2d57e0214baec0c73c105a451

cmd: probe: rework to argp and common connection struct
The option `-i` will be dropped and replaced with the common connection
struct that's being parsed after the `via` word.

Signed-off-by: Tan Siewert <tan@siewert.io>

src/cmd/probe.c
@@ -3,88 +3,99 @@
 
 #include "cmd.h"
 #include "compiler.h"
+#include "connection.h"
 #include "host.h"
 #include "log.h"
 #include "soc.h"
 
-#include <getopt.h>
-#include <stdbool.h>
+#include <argp.h>
 #include <stdio.h>
 
-static void
-cmd_probe_help(const char *name, int argc __unused, char *argv[] __unused)
-{
-    static const char *probe_help =
-        "Usage:\n"
-        "%s probe --help\n"
-        "%s probe --interface INTERFACE ...\n"
-        "%s probe --list-interfaces\n"
-        "%s probe --require <integrity|confidentiality>\n";
+static char cmd_probe_args_doc[] = "[-l] [-r REQUIREMENT] [via DRIVER [INTERFACE [IP PORT USERNAME PASSWORD]]]";
+static char cmd_probe_doc[] =
+    "\n"
+    "Probe command"
+    "\v"
+    "Supported requirements:\n"
+    "  integrity        Require integrity\n"
+    "  confidentiality  Require confidentiality\n";
 
-    printf(probe_help, name, name, name, name);
+static struct argp_option cmd_probe_options[] = {
+    {"list-interfaces", 'l', 0, 0, "List available interfaces", 0},
+    {"require", 'r', "REQUIREMENT", 0, "Requirement to probe for", 0},
+    {0},
+};
+
+struct cmd_probe_args {
+    int list_ifaces;
+    enum bridge_mode requirement;
+    struct connection_args connection;
+};
+
+static error_t cmd_probe_parse_opt(int key, char *arg, struct argp_state *state)
+{
+    struct cmd_probe_args *arguments = state->input;
+    int rc = 0;
+
+    switch (key) {
+    case 'l':
+        arguments->list_ifaces = 1;
+        break;
+    case 'r':
+        if (strcmp("integrity", arg) && strcmp("confidentiality", arg))
+            argp_error(state, "Invalid requirement '%s'", arg);
+
+        if (!strcmp("integrity", arg))
+            arguments->requirement = bm_restricted;
+        else
+            arguments->requirement = bm_disabled;
+        break;
+    case ARGP_KEY_ARG:
+        if (!strcmp(arg, "via")) {
+            rc = cmd_parse_via(state->next - 1, state, &arguments->connection);
+            if (rc != 0)
+                argp_error(state, "Failed to parse connection arguments. Returned code %d", rc);
+            return 0;
+        }
+
+        break;
+    case ARGP_KEY_END:
+        if (!arguments->requirement)
+            arguments->requirement = bm_permissive;
+
+        if (!arguments->connection.interface)
+            arguments->connection.interface = NULL;
+        break;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
 }
 
-static int do_probe(const char *name, int argc, char *argv[])
+static struct argp cmd_probe_argp = {
+    .options = cmd_probe_options,
+    .parser = cmd_probe_parse_opt,
+    .args_doc = cmd_probe_args_doc,
+    .doc = cmd_probe_doc,
+};
+
+static int do_probe(int argc, char **argv)
 {
-    enum bridge_mode required = bm_permissive;
     struct host _host, *host = &_host;
     struct soc _soc, *soc = &_soc;
     enum bridge_mode discovered;
-    bool opt_list_ifaces = false;
-    char *opt_iface = NULL;
     struct ahb *ahb;
     int rc;
 
-    while (1) {
-        int option_index = 0;
-        int c;
-
-        static struct option long_options[] = {
-            { "help", no_argument, NULL, 'h' },
-            { "interface", required_argument, NULL, 'i' },
-            { "list-interfaces", no_argument, NULL, 'l' },
-            { "require", required_argument, NULL, 'r' },
-            { },
-        };
-
-        c = getopt_long(argc, argv, "hi:lr:", long_options, &option_index);
-        if (c == -1)
-            break;
-
-        switch (c) {
-            case 'h':
-                cmd_probe_help(name, argc, argv);
-                rc = EXIT_SUCCESS;
-                goto done;
-            case 'i':
-                opt_iface = optarg;
-                break;
-            case 'l':
-                opt_list_ifaces = true;
-                break;
-            case 'r':
-            {
-                if (!strcmp("confidentiality", optarg)) {
-                    required = bm_disabled;
-                } else if (!strcmp("integrity", optarg)) {
-                    required = bm_restricted;
-                } else {
-                    loge("Unrecognised requirement: %s\n", optarg);
-                    loge("Valid requirements:\n"
-                         "integrity\n"
-                         "confidentiality\n");
-                    rc = EXIT_FAILURE;
-                    goto done;
-                }
-                break;
-            }
-            case '?':
-                rc = EXIT_FAILURE;
-                goto done;
-        }
+    struct cmd_probe_args arguments = {0};
+    rc = argp_parse(&cmd_probe_argp, argc, argv, ARGP_IN_ORDER, 0, &arguments);
+    if (rc != 0) {
+        rc = EXIT_FAILURE;
+        goto done;
     }
 
-    if ((rc = host_init(host, argc - optind, &argv[optind])) < 0) {
+    if ((rc = host_init(host, &arguments.connection) < 0)) {
         loge("Failed to initialise host interfaces: %d\n", rc);
         rc = EXIT_FAILURE;
         goto done;
@@ -101,17 +112,17 @@ static int do_probe(const char *name, int argc, char *argv[])
         goto cleanup_host;
     }
 
-    if (opt_list_ifaces) {
+    if (arguments.list_ifaces) {
         soc_list_bridge_controllers(soc);
         rc = EXIT_SUCCESS;
     } else {
-        if ((rc = soc_probe_bridge_controllers(soc, &discovered, opt_iface)) < 0) {
+        if ((rc = soc_probe_bridge_controllers(soc, &discovered, arguments.connection.interface)) < 0) {
             loge("Failed to probe SoC bridge controllers: %d\n", rc);
             rc = EXIT_FAILURE;
             goto cleanup_soc;
         }
 
-        rc = (required <= discovered) ? EXIT_SUCCESS : EXIT_FAILURE;
+        rc = (arguments.requirement <= discovered) ? EXIT_SUCCESS : EXIT_FAILURE;
     }
 
 cleanup_soc:
@@ -125,8 +136,8 @@ done:
 }
 
 static const struct cmd probe_cmd = {
-    "probe",
-    "[INTERFACE [IP PORT USERNAME PASSWORD]]",
-    do_probe,
+    .name = "probe",
+    .description = "Probe for any BMC",
+    .fn = do_probe,
 };
 REGISTER_CMD(probe_cmd);

commit 723038075ad3e14a1172314c0854f8fd2f5968d2

probe: remove obsolete bridge name
With the migration to argp and a generic connection struct the bridge
name argument became obsolete, but wasn't removed. Unfortunately, the
passed argument "interface" is not the bridge name, but e.g. the UART
device-path, which in the end meant that any driver that didn't match
the path was skipped.

Remove the argument as it isn't properly used anymore after the
migration.

Fixes: a120c4f ("cmd: probe: rework to argp and common connection
                 struct")
Signed-off-by: Tan Siewert <tan.siewert@9elements.com>

src/cmd/probe.c
@@ -121,9 +121,7 @@ static int do_probe(int argc, char **argv)
 		soc_list_bridge_controllers(soc);
 		rc = EXIT_SUCCESS;
 	} else {
-		if ((rc = soc_probe_bridge_controllers(
-			     soc, &discovered,
-			     arguments.connection.interface)) < 0) {
+		if ((rc = soc_probe_bridge_controllers(soc, &discovered)) < 0) {
 			loge("Failed to probe SoC bridge controllers: %d\n",
 			     rc);
 			rc = EXIT_FAILURE;

src/soc.c
@@ -638,8 +638,7 @@ void soc_list_bridge_controllers(struct soc *ctx)
 	}
 }
 
-int soc_probe_bridge_controllers(struct soc *ctx, enum bridge_mode *discovered,
-				 const char *name)
+int soc_probe_bridge_controllers(struct soc *ctx, enum bridge_mode *discovered)
 {
 	enum bridge_mode current, aggregate;
 	struct bridgectl *bridge;
@@ -651,10 +650,6 @@ int soc_probe_bridge_controllers(struct soc *ctx, enum bridge_mode *discovered,
 	error = 0;
 	list_for_each(&ctx->bridges, bridge, entry) {
 		int rc;
-
-		if (name && strcmp(name, bridgectl_name(bridge))) {
-			continue;
-		}
 
 		/* Write the report to stdout */
 		if ((rc = bridgectl_report(bridge, 1, &current)) < 0) {

src/soc.h
@@ -167,6 +167,5 @@ void soc_bridge_controller_unregister(struct soc *soc,
 				      struct bridgectl *bridge);
 
 void soc_list_bridge_controllers(struct soc *soc);
-int soc_probe_bridge_controllers(struct soc *soc, enum bridge_mode *discovered,
-				 const char *name);
+int soc_probe_bridge_controllers(struct soc *soc, enum bridge_mode *discovered);
 #endif